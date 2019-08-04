188c9ca556b8bd4ff5037d6222b7f0a14b41bdb9
Albin Bernhardsson <catscratchesp@gmail.com>
2019-08-04 16:34:03 +0200

    Message box animation
    Animate the entrance of the message box.

--- scripts/message_box.gd
+++ scripts/message_box.gd
@@ -8,6 +8,7 @@ onready var paw_node = get_node("MessageBox/BoxSprite/Label/PawSprite")
 onready var cat_node = get_node("MessageBox/CatSprite")
 onready var box_node = get_node("MessageBox/BoxSprite")
 onready var label_node = get_node("MessageBox/BoxSprite/Label")
+onready var animation_player = get_node("MessageBox/AnimationPlayer")
 
 export(Texture) var cat_neutral_texture
 export(Texture) var cat_facepaw_texture
@@ -22,24 +23,30 @@ class Message:
 var messages = []
 var current_message = 0
 
-# Todo: Callback when having played all messages.
+var callback_object = null
+var callback_method = ""
+
+# State machine.
+enum { ENTER, SHOW_MESSAGES, LEAVE }
+var state = ENTER
 
 # Called when the node enters the scene tree for the first time.
 func _ready():
 	add_message(CAT_FACEPAW, "Don't tell me...\nThis is supposed to be riveting dialogue?")
 	add_message(CAT_SHOW_TEETH, "This makes me so mad!")
-	#show_messages()
+	show_messages(self, "test_callback")
 
 # Called every frame. 'delta' is the elapsed time since the previous frame.
 func _process(delta):
-	if Input.is_action_just_pressed("ui_accept"):
-		# Show next message.
-		current_message += 1
-		if current_message < messages.size():
-			show_current_message()
-		else:
-			visible = false
-			# TODO Callback
+	if state == SHOW_MESSAGES:
+		if Input.is_action_just_pressed("ui_accept"):
+			# Show next message.
+			current_message += 1
+			if current_message < messages.size():
+				show_current_message()
+			else:
+				state = LEAVE
+				animation_player.play_backwards("Enter")
 
 # Add a new message to the queue.
 func add_message(icon, message):
@@ -66,6 +73,30 @@ func set_icon(icon):
 		cat_node.texture = cat_sweat_texture
 
 # Show the queue of messages.
-func show_messages():
+func show_messages(call_object, call_method):
+	if messages.size() == 0:
+		push_warning("Tried to show empty message queue! Queue messages first!")
+	
+	callback_object = call_object
+	callback_method = call_method
+	set_icon(messages[0].icon)
+	
+	animation_player.connect("animation_finished", self, "animation_finished")
+	animation_player.play("Enter")
 	current_message = 0
 	visible = true
+	label_node.set_text("")
+	paw_node.visible = false
+	state = ENTER
+
+# Handle the finished animation.
+func animation_finished(e):
+	if state == ENTER:
+		state = SHOW_MESSAGES
+		show_current_message()
+	elif state == LEAVE :
+		visible = false
+		callback_object.call(callback_method)
+
+func test_callback():
+	print("It worked")
